58ee003249f597bd7850b64287e02bbc2e83cc5f
OsaiDaBoss <osaidsamman@gmail.com>
2023-02-10 13:05:34 -0600

    Assignments for 319

---
+++ Assignments/Activity06/oasamman_Activity06_arrays.js
@@ -0,0 +1,16 @@
+/*
+Osaid Samman
+Feb 8, 2023
+Activity06 - Arrays
+*/
+
+// Q1 : Index of each color
+colors = ['red', 'blue', 'orange’]
+e.g., console.log(colors[0]);
+
+// Q2 : Add a new element 'green'
+colors[3] = 'green';
+console.log(colors);
+
+// Q3 : Lenght of the Array
+console.log(colors.length);
